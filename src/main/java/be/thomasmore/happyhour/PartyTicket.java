3915941ea3d57e1234c93e65c019308c028c6ee2
momoV9 <r0905334@student.thomasmore.be>
2022-11-07 14:42:34 +0100

    nrs 2 = amountblue onnodig

--- src/main/java/be/thomasmore/happyhour/PartyTicket.java
+++ src/main/java/be/thomasmore/happyhour/PartyTicket.java
@@ -11,7 +11,6 @@ public class PartyTicket {
     }
 
     public double buyTickets(int amountRed, int amountBlue) {
-        int nrs2 = amountBlue;
         double v = 0;
         int a = 0;
         int b = 0;
@@ -19,8 +18,8 @@ public class PartyTicket {
         if (amountRed >= 10) {
             a = amountRed - 1;
             v = v + a * PRICE_RED;
-            if (10 > nrs2) {
-                b = nrs2;
+            if (10 > amountBlue) {
+                b = amountBlue;
                 v = v + b * PRICE_BLUE;
                 if (ok) {
                     v = v - a * (double) PRICE_RED / 5;
@@ -28,8 +27,8 @@ public class PartyTicket {
                 } else
                     discount = 0;
             } else {
-                v = v + (nrs2 - 1) * PRICE_BLUE;
-                b = nrs2 - 1;
+                v = v + (amountBlue - 1) * PRICE_BLUE;
+                b = amountBlue - 1;
                 if (ok) {
                     v = v - a * (double) PRICE_RED / 5;
                     v = v - b * (double) PRICE_BLUE / 5;
@@ -41,18 +40,18 @@ public class PartyTicket {
             }
         } else {
             a = amountRed;
-            b = nrs2;
+            b = amountBlue;
             v = v + amountRed * PRICE_RED;
-            if (10 > nrs2 ) {
-                v = v + nrs2 * PRICE_BLUE;
+            if (10 > amountBlue) {
+                v = v + amountBlue * PRICE_BLUE;
                 if (ok) {
                     v = v - a * (double) PRICE_RED / 5;
                     v = v - b * (double) PRICE_BLUE / 5;
 
                 }  else discount +=20;
             } else {
-                v = v + (nrs2 - 1) * PRICE_BLUE;
-                b = nrs2 - 1;
+                v = v + (amountBlue - 1) * PRICE_BLUE;
+                b = amountBlue - 1;
                 if (ok) {
                     v = v - a * (double) PRICE_RED / 5;
                     v = v - b * (double) PRICE_BLUE / 5;

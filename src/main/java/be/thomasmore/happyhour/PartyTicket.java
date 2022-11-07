4e012d938e46f62a6be5a9220cfef6e6f4bc1d3b
momoV9 <r0905334@student.thomasmore.be>
2022-11-07 14:40:38 +0100

    int nrs = amountRed is onnodig

--- src/main/java/be/thomasmore/happyhour/PartyTicket.java
+++ src/main/java/be/thomasmore/happyhour/PartyTicket.java
@@ -11,14 +11,13 @@ public class PartyTicket {
     }
 
     public double buyTickets(int amountRed, int amountBlue) {
-        int nrs = amountRed;
         int nrs2 = amountBlue;
         double v = 0;
         int a = 0;
         int b = 0;
         double discount = 20;
-        if (nrs >= 10) {
-            a = nrs - 1;
+        if (amountRed >= 10) {
+            a = amountRed - 1;
             v = v + a * PRICE_RED;
             if (10 > nrs2) {
                 b = nrs2;
@@ -41,16 +40,16 @@ public class PartyTicket {
                 }
             }
         } else {
-            a = nrs;
+            a = amountRed;
             b = nrs2;
-            v = v + nrs * PRICE_RED;
+            v = v + amountRed * PRICE_RED;
             if (10 > nrs2 ) {
                 v = v + nrs2 * PRICE_BLUE;
                 if (ok) {
                     v = v - a * (double) PRICE_RED / 5;
                     v = v - b * (double) PRICE_BLUE / 5;
-                    discount +=20;
-                }
+
+                }  else discount +=20;
             } else {
                 v = v + (nrs2 - 1) * PRICE_BLUE;
                 b = nrs2 - 1;
